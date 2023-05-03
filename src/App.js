e942701048702774b6e24987f241ddbe3576baf5
Byron Omondi <omondibyron25@gmail.com>
2023-05-03 18:13:44 +0300

    fix delete

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import ProductList from "./components/ProductList";
 import { useState, useEffect } from "react";
-import "./App.css";
 import CartList from "./components/CartList";
 import UpdateProduct from "./components/UpdateProduct";
 import { Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 
 function App() {
   const [product, setProduct] = useState([]);
   const [cart, setCart] = useState([]);
   const [showCart, setShowCart] = useState(false);
-  const [category, setCategory] = useState("all"); // Added category state
+  const [category, setCategory] = useState("all");
 
   const addProduct = (newproduct) => {
     setProduct([...product, newproduct])
@@ -32,15 +32,15 @@ function App() {
     setShowCart(value);
   };
 
-  const handleCategoryChange = (event) => { // Added category change handler
+  const handleCategoryChange = (event) => {
     setCategory(event.target.value);
   }
 
-  const filteredProducts = category === "all" ? product : product.filter(p => p.category === category); // Filter products by category
+  const filteredProducts = category === "all" ? product : product.filter(p => p.category === category);
 
   return (
-    <div>
-      <Header count={cart.length} handleShow={handleShow} onCategoryChange={handleCategoryChange}/> {/* Added onCategoryChange prop */}
+    <div className="container-fluid">
+      <Header count={cart.length} handleShow={handleShow} onCategoryChange={handleCategoryChange}/>
       <Main>
         <Routes>
           <Route path="/" element={<ProductList product={filteredProducts} addToCart={addToCart} addProduct={addProduct}/>}/>
@@ -50,7 +50,7 @@ function App() {
       {showCart ? (
         <CartList cart={cart} />
       ) : (
-        <ProductList product={filteredProducts} addToCart={addToCart} addProduct={addProduct}/> // Pass filtered products to ProductList
+        <ProductList product={filteredProducts} addToCart={addToCart} addProduct={addProduct}/>
       )}
     </div>
   );
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,33 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-function ProductList({product, addToCart}) {
-  let [products, setProduct] = useState()
-  function handleDelete(id=1){
+function ProductList({ product, addToCart }) {
+  let [products, setProduct] = useState(product);
+  
+  function handleDelete(id) {
     fetch(`http://localhost:8000/electronics/${id}`, {
-      method:"DELETE",
+      method: 'DELETE'
     })
     .then(res => res.json())
-    .then(data => setProduct())
+    .then(data => {
+      setProduct(products.filter(product => product.id !== id));
+    })
+    .catch(error => console.log(error));
   }
+
   return (
     <div className='flex'>
-      {
-product.map((productItem, productIndex)=>{
-return(
-<div style={{width: '50%'}}>
-    <div className='product-item'>
-        <img src={productItem.img} width="100%"/>
-        <p>{productItem.productname}|Kshs {productItem.price}</p>
-        <p>{productItem.category}|{productItem.quantity}</p>
-        <button onClick={()=>addToCart(productItem)}>Add To Cart</button>
-        <button onClick={handleDelete}>Delete</button>
+      {products.map((productItem, productIndex) => {
+        return (
+          <div style={{ width: '50%' }}>
+            <div className='product-item'>
+              <img src={productItem.img} width='100%' alt='product' />
+              <p>
+                {productItem.productname} | Kshs {productItem.price}
+              </p>
+              <p>
+                {productItem.category} | {productItem.quantity}
+              </p>
+              <button onClick={() => addToCart(productItem)}>Add To Cart</button>
+              <button onClick={() => handleDelete(productItem.id)}>Delete</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
-</div>
-)
-})
-      }
-    </div>
-  )
+  );
+}
 
-    }
-export default ProductList
+export default ProductList;
